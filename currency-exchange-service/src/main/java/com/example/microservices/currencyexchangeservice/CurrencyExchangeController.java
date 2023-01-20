cdb6ec88b9e36945d174c4e84bccf7928c1873b5
fidelisfelipe <atosfiel@gmail.com>
2023-01-20 21:38:25 +0000

    currency-exchange-service

---
+++ currency-exchange-service/src/main/java/com/example/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,27 @@
+package com.example.microservices.currencyexchangeservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+    @Autowired
+    private ExchangeValueRepository exchangeValueRepository;
+    @Autowired
+    private Environment environment;
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public ExchangeValue retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to){
+        ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to);
+
+        if(exchangeValue == null){
+            return new ExchangeValue(0l, from, to, BigDecimal.ZERO);
+        }
+
+        exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+
+        return exchangeValue;
+    }
+}
---
+++ currency-exchange-service/src/main/java/com/example/microservices/currencyexchangeservice/ExchangeValue.java
@@ -0,0 +1,68 @@
+package com.example.microservices.currencyexchangeservice;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+@Entity(name = "exchange_value")
+public class ExchangeValue {
+    @Id
+    private Long id;
+    @Column(name = "currency_from")
+    private String from;
+    @Column(name = "currency_to")
+    private String to;
+    @Column(name = "conversion_multiple")
+    private BigDecimal conversionMultiple;
+    private int port;
+
+    public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+    }
+    public ExchangeValue(){
+
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return conversionMultiple;
+    }
+
+    public void setConversionMultiple(BigDecimal conversionMultiple) {
+        this.conversionMultiple = conversionMultiple;
+    }
+}
