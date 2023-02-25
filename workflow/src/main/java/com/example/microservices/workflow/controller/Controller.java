93b957285adb8d5eb41ba1288a3e6c01aa9497fb
fidelisfelipe <atosfiel@gmail.com>
2023-02-25 13:49:56 +0000

    add listener and interceptor.

--- workflow/src/main/java/com/example/microservices/workflow/controller/Controller.java
+++ workflow/src/main/java/com/example/microservices/workflow/controller/Controller.java
@@ -3,11 +3,13 @@ package com.example.microservices.workflow.controller;
 import com.example.microservices.workflow.bean.Fluxo;
 import com.example.microservices.workflow.bean.FluxoEvents;
 import com.example.microservices.workflow.bean.FluxoStates;
+import com.example.microservices.workflow.interceptor.StateMachineInterceptor;
 import com.example.microservices.workflow.repository.FluxoRepository;
 import com.example.microservices.workflow.service.FluxoServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.statemachine.StateMachine;
@@ -16,11 +18,16 @@ import org.springframework.statemachine.state.State;
 import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.statemachine.support.StateMachineInterceptorAdapter;
 import org.springframework.statemachine.transition.Transition;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.CoreSubscriber;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Slf4j
 @RestController
@@ -30,10 +37,18 @@ public class Controller {
     private final FluxoRepository repository;
     private final StateMachineFactory<FluxoStates, FluxoEvents> stateMachineFactory;
 
+    private final StateMachineInterceptor stateMachineInterceptor;
+
     @GetMapping("state/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public void state(@PathVariable Long id){
+    public ResponseEntity state(@PathVariable Long id){
         log.info("id: {}", id);
+
+        Optional<Fluxo> fluxo = repository.findById(id);
+
+        if(fluxo.isPresent())
+            return ResponseEntity.status(CREATED).body(fluxo);
+
+            return ResponseEntity.status(NOT_FOUND).body(String.format("not found id:%s", id));
     }
     @GetMapping("create")
     @ResponseStatus(HttpStatus.CREATED)
@@ -50,8 +65,8 @@ public class Controller {
         StateMachine<FluxoStates, FluxoEvents> sm = build(fluxo);
         sm.getExtendedState().getVariables().put(FluxoServiceImpl.DATA_TYPE,fluxo.getDataType());
         var msg = MessageBuilder.withPayload(FluxoEvents.valueOf(fluxo.getEvent()))
-                .setHeader("fluxoId",fluxo.getId())
-                .setHeader("state",fluxo.getState())
+                .setHeader(FluxoServiceImpl.ID_FLUXO,fluxo.getId())
+                .setHeader(FluxoServiceImpl.STATE,fluxo.getState())
                 .build();
 
         sm.sendEvent(msg);
@@ -64,27 +79,7 @@ public class Controller {
         stateMachine.stop();
         stateMachine.getStateMachineAccessor()
                 .doWithAllRegions(sma -> {
-
-                    sma.addStateMachineInterceptor(new StateMachineInterceptorAdapter<>() {
-                        @Override
-                        public void preStateChange( State<FluxoStates,
-                                                    FluxoEvents> state,
-                                                    Message<FluxoEvents> message,
-                                                    Transition<FluxoStates,
-                                                    FluxoEvents> transition,
-                                                    StateMachine<FluxoStates,
-                                                    FluxoEvents> stateMachine,
-                                                    StateMachine<FluxoStates,
-                                                    FluxoEvents> rootStateMachine) {
-                            var orderId = Long.class.cast(message.getHeaders().get("fluxoId"));
-                            var order =  repository.findById(orderId);
-                            if(order.isPresent()){
-                                order.get().setState(state.getId().name());
-                                repository.save(order.get());
-                            }
-                        }
-                    });
-
+                    sma.addStateMachineInterceptor(stateMachineInterceptor);
                     sma.resetStateMachine(new DefaultStateMachineContext<>(FluxoStates.valueOf(entity.get().getState()), null, null, null));
                 });
         stateMachine.start();
--- workflow/src/main/java/com/example/microservices/workflow/service/FluxoServiceImpl.java
+++ workflow/src/main/java/com/example/microservices/workflow/service/FluxoServiceImpl.java
@@ -2,4 +2,6 @@ package com.example.microservices.workflow.service;
 
 public class FluxoServiceImpl implements FluxoService {
     public static final String DATA_TYPE = "dataType";
+    public static final String ID_FLUXO = "idFluxo";
+    public static final String STATE = "state";
 }
