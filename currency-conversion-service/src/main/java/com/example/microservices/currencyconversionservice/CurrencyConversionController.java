918337ff2a6e46b8cf152a05b7729580b1d6253a
fidelisfelipe <atosfiel@gmail.com>
2023-03-08 10:06:34 +0000

    update version api gateway and referency in HELP.md

--- currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -24,21 +24,21 @@ public class CurrencyConversionController {
     @Autowired
     private CurrencyExchangeServiceProxy proxy;
 
-    @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversionBean convertCurrency(@PathVariable("from") String from,
                                                   @PathVariable("to") String to,
                                                   @PathVariable("quantity") BigDecimal quantity){
         Map<String, String> uriVariables = new HashMap<>();
         uriVariables.put("from", from);
         uriVariables.put("to", to);
-        ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity("http://localhost:8765/currency-exchange-service/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class, uriVariables);
+        ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class, uriVariables);
         CurrencyConversionBean response = responseEntity.getBody();
 
         return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(),
                 quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
     }
 
-    @GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversionBean convertCurrencyFeign(@PathVariable("from") String from,
                                                   @PathVariable("to") String to,
                                                   @PathVariable("quantity") BigDecimal quantity){
--- spring-api-gateway-server/src/main/java/com/example/microservices/gateway/LoggingGlobalPreFilter.java
+++ spring-api-gateway-server/src/main/java/com/example/microservices/gateway/LoggingGlobalPreFilter.java
@@ -1,38 +1,23 @@
 package com.example.microservices.gateway;
 
-import com.netflix.zuul.ZuulFilter;
-import com.netflix.zuul.context.RequestContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
-import javax.servlet.http.HttpServletRequest;
 
 @Component
-public class LoggingGlobalPreFilter extends ZuulFilter {
+public class LoggingGlobalPreFilter implements GlobalFilter {
 
     private Logger logger = LoggerFactory.getLogger(LoggingGlobalPreFilter.class);
     @Override
-    public boolean shouldFilter() {
-        return true;
-    }
-
-    @Override
-    public Object run() {
-        HttpServletRequest request =
-                RequestContext.getCurrentContext().getRequest();
-        logger.info("request -> {} request uri -> {}",
-                request, request.getRequestURI());
-        return null;
-    }
-
-    @Override
-    public String filterType() {
-        return "pre";
-    }
-
-    @Override
-    public int filterOrder() {
-        return 1;
+    public Mono<Void> filter(ServerWebExchange exchange,
+                             GatewayFilterChain chain) {
+        logger.info("Path of the request received -> {}",
+                exchange.getRequest().getPath());
+        return chain.filter(exchange);
     }
 }
